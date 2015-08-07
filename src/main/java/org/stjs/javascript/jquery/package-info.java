b0cae0cb3d1c536c6c20d06337c91e6e4b2f78e2
Stéphane Goetz <stephane.goetz@onigoetz.ch>
2015-08-07 17:06:04 +0200

    Update dependencies and add webjars

--- src/main/java/org/stjs/javascript/jquery/package-info.java
+++ src/main/java/org/stjs/javascript/jquery/package-info.java
@@ -1,3 +1,2 @@
-@org.stjs.javascript.annotation.STJSBridge(sources = "/jquery/jquery.js")
+@org.stjs.javascript.annotation.STJSBridge(sources = "webjar:/jquery.min.js")
 package org.stjs.javascript.jquery;
-
